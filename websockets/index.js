c6aae50a8d0a464881bc3f22a13b1494ae6d374a
selmaamc <65399617+selmaamc@users.noreply.github.com>
2020-11-14 22:18:36 +0100

    Add files via upload

---
+++ websockets/index.js
@@ -0,0 +1,23 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/html.html');
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+  });
+});
+io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' }); // This will emit the event to all connected sockets
